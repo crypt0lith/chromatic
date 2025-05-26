3a4ca162699dd67193292d928a8d7ae5ae2e6e8d
crypt0lith <118923461+crypt0lith@users.noreply.github.com>
2025-05-25 19:31:57 -0500

    add local-only submodule linker for `dir()`

--- chromatic/__init__.py
+++ chromatic/__init__.py
@@ -7,6 +7,7 @@ from . import ascii, color, data
 from .ascii import (
     AnsiImage,
     ansi2img,
+    ansify,
     ansi_quantize,
     ascii2img,
     ascii_printable,
@@ -40,3 +41,69 @@ from .color import (
 from .data import register_user_font
 
 __all__ = []
+
+try:
+    import os
+    import sys
+    from functools import lru_cache, wraps
+    from types import ModuleType
+
+    def find_modules(path: str):
+        from setuptools import find_packages
+        from pkgutil import iter_modules
+
+        tree: dict[str, dict | ModuleType] = {__name__: {'__module__': sys.modules[__name__]}}
+        children = set()
+        for pkg in find_packages(path):
+            children.add(pkg)
+            pkg_path = path + '/' + pkg.replace('.', '/')
+            if sys.version_info.major == 2 or (
+                sys.version_info.major == 3 and sys.version_info.minor < 6
+            ):
+                for _, name, ispkg in iter_modules([pkg_path]):
+                    if not ispkg:
+                        children.add(f"{pkg}.{name}")
+            else:
+                for info in iter_modules([pkg_path]):
+                    if not info.ispkg:
+                        children.add(f"{pkg}.{info.name}")
+        for child in children:
+            name = f"{__name__}.{child}"
+            depth = tree
+            for node in name.split('.'):
+                if node not in depth:
+                    depth[node] = {}
+                depth = depth[node]
+            if name in sys.modules:
+                depth['__module__'] = sys.modules[name]
+        return tree
+
+    def publicize_modules(modulename: str, tree: dict[str, dict | ModuleType]):
+        def is_local(obj: object):
+            if isinstance(obj, ModuleType):
+                return obj.__spec__.parent == modulename
+            elif hasattr(obj, '__module__'):
+                return obj.__module__ == modulename
+            return obj is not None
+
+        for name, subtree in tree.items():
+            if name == '__module__' and isinstance(subtree, ModuleType):
+                submodule = subtree
+                init_dir = dir(submodule)
+
+                @wraps(submodule.__dir__)
+                def wrapped():
+                    s = set(attr for attr in init_dir if is_local(getattr(submodule, attr, None)))
+                    if hasattr(submodule, '__all__'):
+                        s.update(submodule.__all__)
+                    return list(s)
+
+                sys.modules[submodule.__name__].__dir__ = wrapped
+
+            else:
+                publicize_modules(f"{modulename}.{name}", subtree)
+
+    publicize_modules(*find_modules(os.path.split(__file__)[0]).popitem())
+
+finally:
+    del find_modules, publicize_modules

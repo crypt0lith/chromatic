dbbcc06c4d2531a0106370b458046aa38141ab68
crypt0lith <118923461+crypt0lith@users.noreply.github.com>
2026-06-22 04:56:51 -0500

    regenerate userfont.pyi stub

--- chromatic/data/userfont.pyi
+++ chromatic/data/userfont.pyi
@@ -7,46 +7,42 @@ __all__ = [
     "unregister_userfont",
     "userfonts",
 ]
-from os import PathLike
+import os
+import typing as tp
+from dataclasses import dataclass, field
 from types import MappingProxyType
-from typing import Final
 
 from PIL.ImageFont import FreeTypeFont
 
+@dataclass(frozen=True, slots=True, repr=False)
 class UserFont:
     font: str
-    size: int = ...
-    index: int = ...
-    encoding: str = ...
-    is_default: bool = ...
-
-    def __init__(
-        self,
-        font: str,
-        *,
-        size: int = ...,
-        index: int = ...,
-        encoding: str = ...,
-        is_default: bool = ...,
-    ): ...
+    size: int = field(default=24, kw_only=True)
+    index: int = field(default=0, kw_only=True)
+    encoding: str = field(default='', kw_only=True)
+    is_default: bool = field(default=False, kw_only=True, compare=False)
+    def __post_init__(self) -> None: ...
+    def __hash__(self) -> int: ...
     def __fspath__(self) -> str: ...
     def to_truetype(self) -> FreeTypeFont: ...
 
-userfonts: Final[MappingProxyType[str, UserFont]]
+userfonts: tp.Final[MappingProxyType[str, UserFont]]
+
+class _RegisterUserfontKwargs(tp.TypedDict, total=False):
+    name: str
+    size: int
+    index: int
+    encoding: str
+    is_default: bool
+    symlink: bool
 
 def register_userfont(
-    fp: str | PathLike[str],
-    font_dir: str | PathLike[str] | None = None,
-    *,
-    name: str = ...,
-    size: int = ...,
-    index: int = ...,
-    encoding: str = ...,
-    is_default: bool = ...,
-    symlink: bool = False,
+    fp: str | os.PathLike[str],
+    font_dir: str | os.PathLike[str] | None = None,
+    **kwargs: tp.Unpack[_RegisterUserfontKwargs],
 ) -> None: ...
-def unregister_userfont(name: str, /, delete=False) -> None: ...
+def unregister_userfont(name: str, /, delete: bool = False) -> None: ...
 def delete_userfont(name: str, /) -> None: ...
 
-VGA437: Final[UserFont]
-DEFAULT_FONT: Final[UserFont]
+VGA437: tp.Final[UserFont]
+DEFAULT_FONT: tp.Final[UserFont]

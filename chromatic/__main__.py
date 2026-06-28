a651ca0e5910443d8231128ced1b3a69f09d892a
crypt0lith <118923461+crypt0lith@users.noreply.github.com>
2026-06-28 07:46:27 -0500

    implement remaining subcommands

--- chromatic/__main__.py
+++ chromatic/__main__.py
@@ -1,12 +1,15 @@
+import collections.abc as abc
 import os
 import sys
 import typing as tp
+from collections import ChainMap
+from inspect import signature
 
 
 def parse_args():
-    import argparse
+    import argparse as ap
 
-    class SetEnvAction(argparse.Action):
+    class SetEnvAction(ap.Action):
         def __init__(self, *args, **kwargs):
             if kwargs.get("nargs") not in ("?", None):
                 raise ValueError(
@@ -29,7 +32,7 @@ def parse_args():
             else:
                 vars(namespace)[self.dest] = os.environ[self.env] = values
 
-    class EnvHelpFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    class EnvHelpFormatter(ap.ArgumentDefaultsHelpFormatter):
         def _get_help_string(self, action):
             out = super()._get_help_string(action)
             if out is not None and isinstance(action, SetEnvAction):
@@ -38,19 +41,11 @@ def parse_args():
                 )
             return out
 
-    parser_kw = {
-        "allow_abbrev": False,
-        "argument_default": argparse.SUPPRESS,
-        "formatter_class": EnvHelpFormatter,
-    }
-
-    new_base_parser = lambda *args, **kwargs: argparse.ArgumentParser(
+    new_base_parser = lambda *args, **kwargs: ap.ArgumentParser(
         *args,
-        **dict(
-            add_help=False,
-            allow_abbrev=False,
-            argument_default=argparse.SUPPRESS,
-            **kwargs,
+        **ChainMap(
+            kwargs,
+            dict(add_help=False, allow_abbrev=False, argument_default=ap.SUPPRESS),
         ),
     )
 
@@ -69,13 +64,13 @@ def parse_args():
     font_dir_env_base = init_font_dir_env_parser()
 
     if os.path.isfile(sys.argv[0]) and os.path.samefile(sys.argv[0], __file__):
-        top_parser = argparse.ArgumentParser(prog=__package__)
+        top_parser = ap.ArgumentParser(prog=__package__)
     else:
-        top_parser = argparse.ArgumentParser()
+        top_parser = ap.ArgumentParser()
 
     cmd_subparsers = top_parser.add_subparsers(dest="cmd", required=True)
 
-    def init_font_subcmds(parser: argparse.ArgumentParser):
+    def init_font_subcmds(parser: ap.ArgumentParser):
         subcmds = parser.add_subparsers(dest="subcmd", required=True)
         mut_by_name_base = new_base_parser()
         mut_by_name_base.add_argument(
@@ -98,7 +93,7 @@ def parse_args():
         uf_attr_opts.add_argument(
             "--default",
             dest="is_default",
-            action=argparse.BooleanOptionalAction,
+            action=ap.BooleanOptionalAction,
             help="whether to set this font as the new default font",
         )
         subcmds.add_parser(
@@ -135,12 +130,12 @@ def parse_args():
             "set-default",
             parents=[font_dir_env_base, mut_by_name_base],
             formatter_class=EnvHelpFormatter,
-            help=(f"""\
-                set a user font as the default font.
-                it will be accessible as `{__package__}.DEFAULT_FONT`"""),
+            help=f"""\
+            set a user font as the default font.
+            it will be accessible as `{__package__}.DEFAULT_FONT`""",
         )
         p_edit_font_opts = subcmd_p_edit.add_argument_group(
-            "font options", argument_default=argparse.SUPPRESS
+            "font options", argument_default=ap.SUPPRESS
         )
         p_edit_font_opts.add_argument(
             "--font", dest="font", metavar="FILE", help="path to truetype font file"
@@ -156,7 +151,21 @@ def parse_args():
             dest="font", metavar="FONT", help="path to truetype font file"
         )
         subcmd_p_register.add_argument(
-            dest="name", metavar="NAME", nargs="?", help="name of the registered font"
+            dest="name",
+            metavar="NAME",
+            nargs="?",
+            default=ap.SUPPRESS,
+            help="name of the registered font",
+        )
+        p_register_fs_opts = subcmd_p_register.add_argument_group("filesystem options")
+        p_register_fs_opts.add_argument(
+            "-s",
+            "--symbolic",
+            dest="symlink",
+            action="store_true",
+            help="""\
+            create a symlink instead of copying the file.
+            like `ln -s TARGET LINK_NAME`, this will fail if the link name already exists""",
         )
         subcmd_p_rename.add_argument(
             dest="newname",
@@ -164,9 +173,281 @@ def parse_args():
             help="new name to give the registered font",
         )
 
-    def init_image_subcmds(parser: argparse.ArgumentParser):
+    def init_image_subcmds(parser: ap.ArgumentParser):
         subcmds = parser.add_subparsers(dest="subcmd", required=True)
-        pass
+        ansify_base = new_base_parser()
+        font_opts = ansify_base.add_argument_group("font options")
+        font_opts.add_argument(
+            "--font",
+            dest="font",
+            help="name of user font, or path to truetype font file",
+        )
+        font_opts.add_argument(
+            "--size",
+            dest="font_size",
+            type=int,
+            metavar="SIZE",
+            help="font size, in pixels",
+        )
+
+        from .color.palette import ColorNamespace
+
+        color_ns = ColorNamespace.asdict()
+
+        def color_parser(allow_rgba=False):
+            def parsed_color(
+                s: str, /
+            ) -> tuple[int, int, int] | tuple[int, int, int, int]:
+                try:
+                    return color_ns[s.strip().replace(" ", "_").upper()]
+                except KeyError:
+                    pass
+                values = tuple(bytes.fromhex(s))
+                if len(values) == 3 or (allow_rgba and len(values) == 4):
+                    return values
+                raise ValueError
+
+            return parsed_color
+
+        color_opts = ansify_base.add_argument_group("color defaults")
+        color_opts.add_argument(
+            "--fg",
+            dest="fg_default",
+            metavar="COLOR",
+            type=color_parser(allow_rgba=True),
+            help="default foreground color. can be hex rgb(a) value or color name",
+        )
+        color_opts.add_argument(
+            "--bg",
+            dest="bg_default",
+            metavar="COLOR",
+            type=color_parser(allow_rgba=True),
+            help="default background color. can be hex rgb(a) value or color name",
+        )
+
+        from_img_base = new_base_parser()
+
+        colorize_opts = from_img_base.add_argument_group("colorization options")
+        colorize_opts.add_argument(
+            "-a",
+            "--ansi-type",
+            metavar="ANSITYPE",
+            choices=("4b", "8b", "24b"),
+            help="""\
+            which ansi colorspace to use for color quantization.
+            since truecolor is rgb, '24b' effectively means 'original colors'
+            (choices: %(choices)s)""",
+        )
+        equalize_opts = colorize_opts.add_mutually_exclusive_group()
+        equalize_opts.add_argument(
+            "--equalize",
+            dest="equalize",
+            action=ap.BooleanOptionalAction,
+            help="""\
+            whether to apply contrast stretch equalization before image-to-ansi conversion""",
+        )
+        equalize_opts.add_argument(
+            "--white-point",
+            dest="equalize",
+            action="store_const",
+            const="white_point",
+            help="apply white-point equalization before image-to-ansi conversion",
+        )
+        colorize_opts.add_argument(
+            "--input-bg",
+            dest="bg",
+            metavar="COLOR",
+            type=color_parser(),
+            default=None,
+            help="""\
+            initial background (canvas) color,
+            injected as an attribute on the intermediate text object itself.
+            can be hex rgb value or color name""",
+        )
+        glyph_opts = from_img_base.add_argument_group("glyph options")
+        glyph_opts.add_argument(
+            "--factor",
+            dest="factor",
+            metavar="N",
+            type=int,
+            help="""\
+            scaling factor for image width to %(metavar)s columns per output line.
+            analogous to level-of-detail""",
+        )
+        char_set_opts = glyph_opts.add_mutually_exclusive_group()
+        char_set_opts.add_argument(
+            "-c",
+            "--chars",
+            dest="char_set",
+            metavar="CHARS",
+            help="""\
+            charset where %(metavar)s is a string containing chars to use for glyph selection.
+            a char can appear more than once, which affects frequency distribution""",
+        )
+
+        from .image._curses import ascii_printable, cp437_printable
+
+        char_set_preset_help = (
+            "use printable characters from {} character encoding as charset"
+        ).format
+        char_set_opts.add_argument(
+            "--ascii",
+            dest="char_set",
+            action="store_const",
+            const=ascii_printable(),
+            help=char_set_preset_help("ASCII"),
+        )
+        char_set_opts.add_argument(
+            "--latin1",
+            dest="char_set",
+            help=char_set_preset_help("Latin-1"),
+            action="store_const",
+            const="".join(filter(str.isprintable, bytes(range(256)).decode("latin1"))),
+        )
+        char_set_opts.add_argument(
+            "--cp437",
+            dest="char_set",
+            action="store_const",
+            const=cp437_printable(),
+            help=char_set_preset_help("code page 437"),
+        )
+        char_set_opts.add_argument(
+            "--cp1252",
+            dest="char_set",
+            help=char_set_preset_help("Windows-1252"),
+            action="store_const",
+            const="".join(
+                filter(
+                    str.isprintable, bytes(range(256)).decode("cp1252", errors="ignore")
+                )
+            ),
+        )
+
+        glyph_sort_opts = glyph_opts.add_mutually_exclusive_group()
+        glyph_sort_opts.add_argument(
+            "--sort",
+            dest="sort_glyphs",
+            action=ap.BooleanOptionalAction,
+            help="""\
+            whether to sort glyphs based on their perceived 'luminance'.
+            for example, ('.' -> '#') is (dark -> light)""",
+        )
+        glyph_sort_opts.add_argument(
+            "-r",
+            "--reverse",
+            dest="sort_glyphs",
+            action="store_const",
+            const=reversed,
+            help="""\
+            sort glyphs in reverse order.
+            flips the luminance mapping to (light -> dark)""",
+        )
+
+        def save_img_callback(path: str | os.PathLike[str] | None = None, /):
+
+            # deferred exception handler
+            # on write error, fall-through to Image.show()
+            # then re-raise after in-memory image is opened
+
+            from functools import wraps
+
+            def defer_exc[**P, R](
+                f: abc.Callable[P, abc.Generator[Exception, tp.Any, R]], /
+            ):
+                @wraps(f)
+                def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+                    err = None
+                    it = f(*args, **kwargs)
+                    try:
+                        while True:
+                            err = next(it)
+                    except StopIteration as stop:
+                        return stop.value
+                    finally:
+                        if err is not None:
+                            raise err
+
+                return wrapper
+
+            import PIL.Image
+
+            @defer_exc
+            def callback(ns: ap.Namespace, img: PIL.Image.Image):
+                if path is not None:
+                    try:
+                        img.save(path)
+                    except Exception as e:
+                        yield e
+                    else:
+                        if getattr(ns, "show", False):
+                            with PIL.Image.open(path) as f:
+                                f.show()
+                        return path
+                dump_stdout = hasattr(ns, "dumpfile") and ns.dumpfile.fileno() == 1
+                if getattr(ns, "show", not dump_stdout):
+                    img.show()
+
+            return callback
+
+        def save_img_to_dir(dirname: str, /):
+            from datetime import datetime
+            from pathlib import Path
+
+            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+            outfile = Path(dirname, f"{__package__}_{timestamp}.png")
+            return save_img_callback(outfile)
+
+        output_opts_base = new_base_parser()
+        output_opts = output_opts_base.add_argument_group("output options")
+        outfile_opts = output_opts.add_mutually_exclusive_group()
+        outfile_opts.add_argument(
+            "-O",
+            "--outfile",
+            metavar="FILE",
+            dest="outfile_callback",
+            type=save_img_callback,
+            help="save the image to %(metavar)s",
+        )
+        outfile_opts.add_argument(
+            "-o",
+            "--output-dir",
+            metavar="DIRECTORY",
+            dest="outfile_callback",
+            type=save_img_to_dir,
+            help="save the image as a png file in %(metavar)s",
+        )
+        output_opts.add_argument(
+            "--show",
+            action=ap.BooleanOptionalAction,
+            help="whether to show the image in the system's image viewer",
+        )
+        dumpfile_opts = output_opts.add_mutually_exclusive_group()
+        dumpfile_opts.add_argument(
+            "-d",
+            "--dump-text",
+            metavar="FILE",
+            dest="dumpfile",
+            type=ap.FileType("wb"),
+            help="write the ansified image text to %(metavar)s",
+        )
+        dumpfile_opts.add_argument(
+            "--stdout",
+            dest="dumpfile",
+            action="store_const",
+            const=sys.stdout.buffer,
+            help="write the ansified image text to stdout",
+        )
+        output_opts_base.set_defaults(_outfile_callback=save_img_callback())
+        subcmd_p_ansify = subcmds.add_parser(
+            "ansify",
+            parents=[font_dir_env_base, ansify_base, from_img_base, output_opts_base],
+            formatter_class=EnvHelpFormatter,
+        )
+        subcmd_p_ansify.add_argument(
+            dest="img",
+            metavar="IMAGEFILE",
+            help="input image",
+        )
 
     font_cmd_subparser = cmd_subparsers.add_parser("font")
     image_cmd_subparser = cmd_subparsers.add_parser("image")
@@ -177,10 +458,39 @@ def parse_args():
     return top_parser.parse_args()
 
 
+def _call_from_ns[R](f: abc.Callable[..., R], /, ns, **kwargs) -> R:
+    params = signature(f).parameters
+    kwargs = ChainMap(kwargs, vars(ns))
+    f_args, f_kwargs = [], {}
+    for k, p in params.items():
+        if k not in kwargs:
+            continue
+        if p.kind == 0:
+            f_args.append(kwargs[k])
+        else:
+            f_kwargs[k] = kwargs[k]
+    return f(*f_args, **f_kwargs)
+
 def handle_image(ns):
+    vars(ns).setdefault("outfile_callback", ns._outfile_callback)
     match ns.subcmd:
+        case "ansify":
+            from .image import img2ansi, ansi2img
+
+            ansi_array = _call_from_ns(img2ansi, ns)
+            if hasattr(ns, "dumpfile"):
+                ns.dumpfile.writelines(f"{s}\n".encode() for s in map("".join, ansi_array))
+            img = _call_from_ns(ansi2img, ns, ansi_array=ansi_array)
+            try:
+                outpath = ns.outfile_callback(ns, img)
+            except Exception as e:
+                print(f"[-] error: {e}", file=sys.stderr)
+                return -1
+            if outpath is not None:
+                print(f"{outpath}")
+            return
         case _:
-            raise NotImplementedError(f"subcommand {ns.subcmd!r} not yet implemented")
+            raise ValueError(f"invalid subcommand: {ns.subcmd!r}")
 
 
 def font_list(ns):
@@ -197,7 +507,7 @@ def font_list(ns):
         table = defaultdict(list)
         widths = defaultdict(int)
         justs = {}
-        anno = uf._userfont_dict_struct()[-1]
+        anno = uf._userfont_dict_struct().annotations
         for name, obj in uf.userfonts.items():
             is_default = obj is uf.DEFAULT_FONT
             for k, v in dict(name=name, **asdict(obj)).items():
@@ -237,7 +547,7 @@ def font_list(ns):
     else:
         out = []
         for name, obj in uf.userfonts.items():
-            d = dict(name=name, **asdict(obj))
+            d: dict[str, tp.Any] = dict(name=name, **asdict(obj))
             d.update(font=str(d.pop("_base_dir").joinpath(d["font"])))
             out.append(d)
         if json_mode:
@@ -245,39 +555,51 @@ def font_list(ns):
 
             outlines = map(json.dumps, out)
         else:
-            outlines = ["\t".join(map(str, d.values())) for d in out]
-
-    return print(*outlines, sep="\n")
+            outlines = ("\t".join(map(str, d.values())) for d in out)
+    return sys.stdout.writelines(f"{line}\n" for line in outlines)
 
 
 def handle_font(ns):
     match ns.subcmd:
         case "list":
             return font_list(ns)
-        case "register":
-            from .data.userfont import register_userfont
+        case ("register" | "edit") as sc:
+            keys = {"size", "index", "encoding", "is_default"}
+            match sc:
+                case "register":
+                    from .data.userfont import register_userfont as f
 
-            kwargs = {
-                k: v
-                for k, v in vars(ns).items()
-                if k
-                in {
-                    "font_dir",
-                    "name",
-                    "size",
-                    "index",
-                    "encoding",
-                    "is_default",
-                    "symlink",
-                }
-            }
-            return register_userfont(ns.font, **kwargs)
-        case "delete":
-            from .data.userfont import delete_userfont
+                    args = [ns.font]
+                    keys.update(("font_dir", "name", "symlink"))
+                case "edit":
+                    from .data.userfont import edit_userfont as f
 
-            return delete_userfont(ns.name)
+                    args = [ns.name]
+                    keys.add("font")
+                case _:
+                    raise RuntimeError("unreachable")
+            kwargs = {}
+            for k in vars(ns).keys() & keys:
+                kwargs[k] = getattr(ns, k)
+            return f(*args, **kwargs)
+        case ("rename" | "set-default" | "delete") as sc:
+            args = [ns.name]
+            match sc:
+                case "rename":
+                    from .data.userfont import rename_userfont as f
+
+                    args.append(ns.newname)
+                case "set-default":
+                    from .data.userfont import set_default_userfont as f
+
+                case "delete":
+                    from .data.userfont import delete_userfont as f
+
+                case _:
+                    raise RuntimeError("unreachable")
+            return f(*args)
         case _:
-            raise NotImplementedError(f"subcommand {ns.subcmd!r} not yet implemented")
+            raise ValueError(f"invalid subcommand: {ns.subcmd!r}")
 
 
 def main():
